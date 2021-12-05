68468a4ccd5896abb901cf39d1a814e68496a17c
Moshiko-Start <moshiko14691@gmail.com>
2021-12-05 13:06:42 +0200

    Updates

---
+++ precache-manifest.86a7ee95b495f8ddc458cc01be183f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abce03a20bbccda7551cb314521a207e",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "dcbf1e83bcca3d75809b",
+    "url": "/quiz-app/static/css/main.50566d9f.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/quiz-app/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbf1e83bcca3d75809b",
+    "url": "/quiz-app/static/js/main.5cb322ed.chunk.js"
+  },
+  {
+    "revision": "2b8e60fa09f000520361",
+    "url": "/quiz-app/static/js/runtime-main.d5202142.js"
+  }
+]);
